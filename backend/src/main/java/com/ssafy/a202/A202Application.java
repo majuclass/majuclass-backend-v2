38f81260ff518276124de21156895640e61a31fd
FlyBirds99 <shb9808@naver.com>
2025-10-16 13:11:30 +0900

    backend initial commit

---
+++ backend/src/main/java/com/ssafy/a202/A202Application.java
@@ -0,0 +1,13 @@
+package com.ssafy.a202;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A202Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A202Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ssafy/a202/A202ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.a202;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A202ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
