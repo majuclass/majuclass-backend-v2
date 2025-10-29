4d18fc64ee53e90d6619b9bdfa5945c41ec898cc
AhiamYeong <toktto@gmail.com>
2025-10-29 11:16:58 +0900

    [FE] Design: 시나리오 플로우 전반적으로 디자인 개선

--- frontend/src/components/scenario/screen/StartScreen.tsx
+++ frontend/src/components/scenario/screen/StartScreen.tsx
@@ -18,8 +18,8 @@ export default function StartScreen({ scenario, onStart }: StartScreenProps) {
   };
 
   return (
-    <div className="h-screen w-screen flex items-center justify-center bg-[url('/snack-bg.jpg')] bg-cover bg-center">
-      <div className="backdrop-blur-sm bg-white/80 rounded-2xl shadow-xl p-10 text-center w-80">
+    <div className="h-screen w-screen flex items-center justify-center bg-cover bg-center">
+      <div className="backdrop-blur-sm bg-white/80 rounded-2xl shadow-xl p-12 text-center w-full max-w-xl">
         <h2 className="text-2xl font-bold mb-2">{scenario.title}</h2>
         <p className="text-lg text-gray-700 mb-1">{scenario.summary}</p>
         {/* TODO: 난이도 매핑 */}
@@ -31,7 +31,7 @@ export default function StartScreen({ scenario, onStart }: StartScreenProps) {
 
         {/* <p>{scenario.categoryName}</p> */}
         <button
-          className="bg-orange-400 text-white py-2 px-6 rounded-md font-semibold hover:bg-orange-500 transition"
+          className="bg-orange-400 text-white py-2 px-6 rounded-md font-normal hover:bg-orange-500 transition tracking-wide"
           onClick={onStart}
         >
           실행하기
