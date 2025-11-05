b3e22c121e38c0ee7944e06f907ece2c0ce3d1bf
심양관 <cash3543@naver.com>
2025-11-05 10:35:27 +0900

    [FE] Hotfix: 링크 미리보기 추가 및 테스트 완료(ngrok)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ["eeb425caf5eb.ngrok-free.app"], // 매번 수정해주어야 함
+  },
 })
