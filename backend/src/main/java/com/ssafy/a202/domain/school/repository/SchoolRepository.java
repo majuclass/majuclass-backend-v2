a708ee1ab7bcfdf2ac23b722ca306479572324b3
SUICHO <65757661+whtndls@users.noreply.github.com>
2025-11-24 03:41:18 +0900

    Remove: Ver 1 제거

--- backend/src/main/java/com/ssafy/a202/domain/school/repository/SchoolRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.a202.domain.school.repository;
-
-import com.ssafy.a202.domain.school.entity.School;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SchoolRepository extends JpaRepository<School, Long> {
-}
