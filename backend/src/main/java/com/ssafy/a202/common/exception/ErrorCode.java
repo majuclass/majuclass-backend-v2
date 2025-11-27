7e713054ff85f6fae5450df4faa0bd9aa6463448
SUICHO <65757661+whtndls@users.noreply.github.com>
2025-11-27 12:32:25 +0900

    Feat: 익셉션 핸들러

--- backend/src/main/java/com/ssafy/a202/common/exception/ErrorCode.java
+++ backend/src/main/java/com/ssafy/a202/common/exception/ErrorCode.java
@@ -1,4 +1,4 @@
-package com.ssafy.a202.common.entity;
+package com.ssafy.a202.common.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,7 +14,10 @@ public enum ErrorCode {
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다."),
     USERNAME_DUPLICATE(HttpStatus.CONFLICT, "이미 사용 중인 아이디입니다."),
     EMAIL_DUPLICATE(HttpStatus.CONFLICT, "이미 사용 중인 이메일입니다."),
-    ORGANIZATION_NOT_FOUND(HttpStatus.NOT_FOUND, "기관을 찾을 수 없습니다.");
+    ORGANIZATION_NOT_FOUND(HttpStatus.NOT_FOUND, "기관을 찾을 수 없습니다."),
+    TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다."),
+    VALIDATION_FAILED(HttpStatus.BAD_REQUEST, "입력 값 검증에 실패했습니다.");
 
     private final HttpStatus status;
     private final String message;
